f7234d14a6a40060533343f433bc022518469924
Shiro Kun <khoirul.20998@gmail.com>
2020-10-27 21:11:14 +0700

    Perbaikan

--- src/components/PesanComponents/CardFile.js
+++ src/components/PesanComponents/CardFile.js
@@ -90,6 +90,10 @@ const CardUploadFile = (props) => {
                         value={props.pagesPrint}
                     />
                     <ContentDetailFile
+                        field="Total Pages"
+                        value={props.totalPages}
+                    />
+                    <ContentDetailFile
                         field="Keterangan"
                         value={props.fileketerangan}
                         line={1}
@@ -125,6 +129,7 @@ const CardFile = () => {
                     filesize="1 MB"
                     pagesPrint="1 s/d 10"
                     printType="Warna"
+                    totalPages="20"
                     fileketerangan="Lorem Ipsum adalah contoh teks atau dummy dalam industri percetakan dan penataan huruf atau typesetting. Lorem Ipsum telah menjadi standar contoh teks sejak tahun 1500an, saat seorang tukang cetak yang tidak dikenal mengambil sebuah kumpulan teks dan mengacaknya untuk menjadi sebuah buku contoh huruf. Ia tidak hanya bertahan selama 5 abad, tapi juga telah beralih ke penataan huruf elektronik, tanpa ada perubahan apapun. Ia mulai dipopulerkan pada tahun 1960 dengan diluncurkannya lembaran-lembaran Letraset yang menggunakan kalimat-kalimat dari Lorem Ipsum, dan seiring munculnya perangkat lunak Desktop Publishing seperti Aldus PageMaker juga memiliki versi Lorem Ipsum."
                     onDeleted={(e) => console.log(e) }
                 />
